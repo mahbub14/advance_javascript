bb72c38d14ad6ca13e2ad4a17ede83258b9595e3
mahbub14 <mahbubhasan14@gmail.com>
2020-04-15 07:50:19 +0600

    learning js advance

---
+++ map-filter.js
@@ -0,0 +1,10 @@
+const numbers = [3, 4, 5, 6];
+const output = [];
+
+for( let i = 0; i =0; i<numbers.length; i++){
+    const element = numbers[i];
+    const result = element*element;
+    output.push(result);
+}
+
+console.log(output);
