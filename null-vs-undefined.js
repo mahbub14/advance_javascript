8148477e710d3494be252b14878f639c081a9778
mahbub14 <mahbubhasan14@gmail.com>
2020-04-15 07:24:45 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+//let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+const result = add (13, 82);
+console.log(result);
+
+function add (num1, num2){
+    console.log(num1, num2);
+}
+const premik = {name: "smart dude", phone: 38593};
+console.log(premik.gf);
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,5,9];
+console.log (ages[11]);
+
