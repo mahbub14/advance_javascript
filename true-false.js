5e6ee6006b54bab19ba657265cd898e8e7f84cdd
mahbub14 <mahbubhasan14@gmail.com>
2020-04-15 07:06:20 +0600

    learning

---
+++ true-false.js
@@ -0,0 +1,19 @@
+const age = 4;
+if (age){
+    console.log ("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+
+//falsy:
+//false
+// 0
+// ""
+// udentifined
+// null
+// NaN
+
+//Truthy:
+// "0" ' ' []
